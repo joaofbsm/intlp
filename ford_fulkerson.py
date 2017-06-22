0f6acda6b891b285b4d0dc0a5b841f7142c405e6
Joao Martins <joaofbsm@hotmail.com>
2017-06-22 01:05:52 -0300

    Add Ford-Fulkerson, Add flow examples

--- ford_fulkerson.py
+++ ford_fulkerson.py
@@ -10,3 +10,96 @@ __version__ = "3.0"
 import sys
 import numpy as np
 
+def create_weighted_matrix(c, N):
+    """Creates matrix with weights based in arch capacity and arch connections.
+    
+    Arguments:
+        c -- Arch capacity vector.
+        N -- Incidence matrix.
+    """
+
+    vertex_num = N.shape[0]
+    # Weighted matrix
+    G = np.zeros((vertex_num, vertex_num))
+
+    for i, vertex in enumerate(N):
+        for j, edge in enumerate(vertex):
+            if edge == -1:
+                # If edge points outward the vertex, add its weight
+                for k in range(vertex_num):
+                    if N[k][j] == 1:
+                        # k is vertex to where the edge points
+                        G[i][k] = c[j]
+                        break
+
+    return G
+
+
+def find_path(s, t, G):
+    """Finds path using depth first search.
+
+    Arguments:
+        s -- Source vertex index.
+        t -- Target(Sink) vertex index.
+        G -- Weighted matrix.
+    """
+
+    visited = np.zeros(G.shape[0], dtype="int")
+    stack = []
+    path = np.zeros(G.shape[0], dtype="int")
+
+    # At the beginning, visit source
+    stack.append(s)
+    visited[s] = 1
+
+    while stack:
+        vertex = stack.pop()
+        for i, capacity in enumerate(G[vertex]):
+            if not visited[i] and capacity > 0:
+                stack.append(i)
+                visited[i] = 1
+                path[i] = vertex
+    return path
+
+
+def solve(c, N):
+    """Solve Ford-Fulkerson for a given vector c and matrix N.
+    
+    Arguments:
+        c -- Arch capacity vector.
+        N -- Incidence matrix.
+    """
+
+    # G combines the forward and backward residual capacities
+    G = create_weighted_matrix(c, N)
+    orig_G = np.copy(G)
+    flow = 0  # Total flow
+    s = 0  # Source
+    t = N.shape[0] - 1  # Target
+
+    path = find_path(s, t, G)
+    while np.count_nonzero(path):
+        path_flow = float("inf")
+        v = t
+        while v != s:
+            if G[path[v]][v] < path_flow:
+                path_flow = G[path[v]][v]
+            v = path[v]
+
+        flow += int(path_flow)
+
+        v = t
+        while v != s:
+            u = path[v]
+            G[u][v] -= path_flow
+            G[v][u] += path_flow
+            v = u
+
+        path = find_path(s, t, G)
+
+    print(flow)
+
+    for i in range(G.shape[0]):
+        for j in range(G.shape[1]):
+            if G[i][j] == 0 and orig_G[i][j] != 0:
+                print(i, "-", j, sep="")
--- main.py
+++ main.py
@@ -12,50 +12,47 @@ import numpy as np
 import set_cover as sc
 import ford_fulkerson as ff
 
-def read_file(file_path, algorithm):
-    f = open(file_path, "r")
-    results = None
-    if algorithm == "set":
-        # Number of vertices
-        n_verts = int(f.readline())  
-        # Number of sets
-        n_sets = int(f.readline())  
-
-        # Get c vector with n_sets entries as a numpy array
-        string_list = f.readline().split(" ")[:n_sets]
-        int_list = [int(x) for x in string_list]
-        c = np.asarray(int_list)
-
-        # Correlation matrix N
-        N = np.empty(shape=(n_verts, n_sets))
-        for i in range(n_verts):
-            string_list = f.readline().split(" ")[:n_sets]
-            int_list = [int(x) for x in string_list]
-            N[i] = np.asarray(int_list)
-        results = (c, N)
-
-    elif algorithm == "flux":
-        pass
-    else:
-        print("[ERROR] Algorithm type does not match any.")
-    
-    f.close()
-    return results
-
+# TODO
+# - Collapse read_file in one procedure
 
 def main(argv):
     if not argv:
-        print("usage: main.py set|flux input_file")
+        print("usage: main.py set|flow input_file")
     else:
         algorithm = argv[0]
         input_file = argv[1]
         results = read_file(input_file, algorithm)
         if algorithm == "set":
             sc.solve(*results)
-        elif algorithm == "flux":
+        elif algorithm == "flow":
             ff.solve(*results)
         else:
             print("Error")
+
+
+def read_file(file_path, algorithm):
+    f = open(file_path, "r")
+    results = None
+
+    # Number of rows
+    m = int(f.readline())  
+    # Number of columns
+    n = int(f.readline())  
+
+    # Get c vector with n entries as a numpy array
+    string_list = f.readline().split(" ")[:n]
+    int_list = [int(x) for x in string_list]
+    c = np.asarray(int_list)
+
+    # Correlation/Incidence matrix N
+    N = np.empty(shape=(m, n))
+    for i in range(m):
+        string_list = f.readline().split(" ")[:n]
+        int_list = [int(x) for x in string_list]
+        N[i] = np.asarray(int_list)
+    results = (c, N)   
+    f.close()
+    return results
 
 
 if __name__ == "__main__":
